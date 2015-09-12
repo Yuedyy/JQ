6bdc9f9487cbf6a6dd99b60ce400e0cf03d8303b
栗昂桀 <liangjie.yuedyy@gmail.com>
2015-09-12 22:59:18 +0800

    整个加载好

--- js/script03.js
+++ js/script03.js
@@ -1,6 +1,3 @@
-$('#img1').load(function(){
-	alert('the img1 has loaded')
-});
-$('#img2').load(function(){
-	alert('the img2 has loaded')
+$(window).load(function(){
+	alert('window loaded');
 });
