73c156a606ff9e2d435f8c0f7407f5c74c3c6480
栗昂桀 <liangjie.yuedyy@gmail.com>
2015-09-12 23:15:41 +0800

    id选择器和类选择器

--- js/script03.js
+++ js/script03.js
@@ -1,3 +1,11 @@
 $(window).load(function(){
 	alert('window loaded');
 });
+
+$(document).ready(function(){
+	alert('the dom is ready');
+});
+
+$('.img').click(function(){
+	alert('picture has clicked')
+})
